a71f5ef33d8e657335f2d8f5f8a93f6a415f127f
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2024-02-15 11:23:02 +0500

    yana yangilandi

--- patch.js
+++ patch.js
@@ -1,12 +1,29 @@
 const petchGrid = document.querySelector(".post-grid");
+const loading = document.querySelector(".custom-loader");
+const dark  = document.querySelector("#dark");
+const fullModal  = document.querySelector("#full-modal");
+const topelement = document.querySelector(".top-elements");
+const body = document.querySelector("body");
 
 const baseUrl = "https://dummyapi.io/data/v1/post";
 const basekey = "65ccbfadb0e06137825c6ec5";
 
-function apis() {
+dark.addEventListener("click", ()=>{
+    dark.classList.toggle("pl-[30px]");
+    topelement.classList.toggle("bg-blue-900");
+    body.classList.toggle("bg-blue-950");
+    body.classList.toggle("text-white");
+    fullModal.classList.toggle("bg-violet-950");
+})
+
+window.addEventListener("load", ()=>{
+    loading.style.display = "none";
+})
+
+async function apis() {
     const ids = new URLSearchParams(window.location.search);
     let openId = ids.get("id");
-    fetch(baseUrl, {
+    await fetch(baseUrl, {
         method: "GET",
         headers: {
             "app-id": basekey,
@@ -19,7 +36,7 @@ function apis() {
                 console.log(date);
                 petchGrid.innerHTML = `<div class="w-[700px] h-[100%] m-auto border-2 rounded-md">
                     <img class="w-[700px] h-[700px] p-[20px]" src="${date.image}" alt="images">
-                    <div class="p-[20px] flex space-y">
+                    <div id="colors" class="p-[20px] flex space-y">
                         <div>
                         <h1 class="font-semibold pl-[10px] pt-[10px] font-sans text-[30px]">${date.text}</h1>
                         <p class="pl-[10px] pt-[5px] font-sans text-[15px] text-stone-600">Published by ${date.owner.firstName} ${date.owner.lastName}</p>
